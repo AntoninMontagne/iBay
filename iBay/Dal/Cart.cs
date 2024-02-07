fd16da5b3f9bf942b3e068e6c104ab0dc2f1f258
lougautr <lougautr@gmail.com>
2024-02-07 09:51:00 +0100

    Add models

---
+++ iBay/Dal/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Cart
+    {
+        public int CartID { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual User User { get; set; }
+
+        public Cart()
+        {
+            Products = new HashSet<Product>();
+        }
+
+
+    }
+}
