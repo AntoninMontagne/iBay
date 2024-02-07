e6af08f6b84974e2e738c87be9c4105b403b4bb7
lougautr <lougautr@gmail.com>
2024-02-07 13:12:45 +0100

    ajout appsettings.json

--- iBay/Dal/obj/Debug/net8.0/Dal.AssemblyInfo.cs
+++ iBay/Dal/obj/Debug/net8.0/Dal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac89dab0ad0b40de62f7eaac3761d29df909ac38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
